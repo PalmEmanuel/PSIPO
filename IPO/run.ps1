006b086cb10589d2fea4de424365e51a9929bdeb
Emanuel Palm <ep__palm@hotmail.com>
2020-09-02 20:29:14 +0200

    aztable

--- IPO/run.ps1
+++ IPO/run.ps1
@@ -5,6 +5,11 @@ param($Request, $TriggerMetadata)
 
 
 
+# 1. Get previously saved IPOs filings from storage table
+# 2. Get S-1 filings from RSS feed
+# 3. Compare the two lists, select only unique new ones
+# 4. Return the results through the output binding
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
