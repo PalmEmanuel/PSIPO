d1517dba39dc9199699185796775fee0d0636b2c
Emanuel Palm <ep__palm@hotmail.com>
2020-09-02 22:00:10 +0200

    Gets all new S-1 filings and adds to table

--- IPO/run.ps1
+++ IPO/run.ps1
@@ -1,17 +1,28 @@
-using namespace System.Net
-
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+# Connect to table using the connection string in the application setting AzureWebJobsStorage
+$Context = New-AzStorageContext -ConnectionString $env:AzureWebJobsStorage
+$Table = Get-AzStorageTable -Context $Context
+# Read all previously stored S1 filings
+$StoredS1s = Get-AzTableRow -Table $Table.CloudTable -PartitionKey 'IPO' | Select-Object Name,Link,Time,Id
 
+# Get all recent S1 filings from SEC
+$Results = Invoke-RestMethod 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&type=S-1&output=atom'
+# Filter out anything not matching exactly S1, create hashtables with properties
+$RecentS1s = $Results | Where-Object { $_.category.term -eq 'S-1' } | ForEach-Object {
+    @{
+        # Use regex to get only the company name from the title
+        Name = $_.title -creplace '.+ - (.+) \(.+\) \(.+\)', '$1'
+        Link = $_.link.href
+        Time = Get-Date $_.updated -Format 'yyyy-MM-dd hh:mm:ss'
+        Id   = $_.id
+    }
+}
 
-# 1. Get previously saved IPOs filings from storage table
-# 2. Get S-1 filings from RSS feed
-# 3. Compare the two lists, select only unique new ones
-# 4. Return the results through the output binding
+# Add all new S1 filings to table not already there
+foreach ($S1 in $RecentS1s.Where({ $_.Id -notin $StoredS1s.Id })) {
+    $S1 | ConvertTo-Json | Write-Host
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
-})
+    $null = Add-AzTableRow -Table $Table.CloudTable -PartitionKey 'IPO' -RowKey $S1.Id -Property $S1
+}
